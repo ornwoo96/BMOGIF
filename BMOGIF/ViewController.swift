d4904645110e9826cc86d5fe32c444e42587f6d4
ornwoo96 <dongou705@naver.com>
2023-02-21 17:35:23 +0900

    Initial Commit

---
+++ BMOGIF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMOGIF
+//
+//  Created by 김동우 on 2023/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
